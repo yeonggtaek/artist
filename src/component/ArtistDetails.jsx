f3bd4ce0d0bfc29af7765b326f43dca0d74e95f7
yeongtaek <144389087+yeonggtaek@users.noreply.github.com>
2024-09-19 16:40:25 -0700

    css

--- src/component/ArtistDetails.jsx
+++ src/component/ArtistDetails.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 function ArtistDetails({ artist }) {
   return (
-    <div style={{ border: '1px solid #E2E2E2', margin: '10px', padding: '10px' }}>
+    <div style={{ border: '1px solid #E2E2E2', margin: '10px', padding: '10px', backgroundColor: '#E2E2E2', display: 'flex', justifyContent: 'center' }}>
       <h2>{artist.name}</h2>
       <img 
-src={artist.photo_url} alt={`${artist.name}`} style={{ width: '200px', height: 'auto'}} />
+src={artist.photo_url} alt={`${artist.name}`} style={{ width: '200px', height: 'auto', display: 'flex', justifyContent: 'center'}} />
       <p>Country: {artist.country}</p>
       <p>Years Active: {artist.years_active}</p>
     </div>
